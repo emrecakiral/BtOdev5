df40671065c48756561d55d7c38abf1bb88ac28e
Emre Çakıral <emrecakiral@outlook.com>
2022-03-04 22:41:43 +0300

    Add project files.

---
+++ BtOdev5/Program.cs
@@ -0,0 +1,28 @@
+﻿class main
+{
+    static void Main(String[] args)
+    {
+        int[] sirasizlar = { 100, 5, 50, 2, 10, 99, 8, 4, 2 };
+        int[] siralanmis = Sirala(sirasizlar); // sırasızlar dizinin siralanmış halini siralnamış dizisine atar...
+        foreach (var item in siralanmis)
+            Console.WriteLine(item);
+    }
+    static int[] Sirala(int[] arr)
+    {
+        int[] sirali = new int[0];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                if (arr[j + 1] >= arr[j])
+                    continue;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        sirali = arr;
+        return sirali;
+    }
+}
